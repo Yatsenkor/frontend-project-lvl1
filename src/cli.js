74c2b88d097a1561970f71fe3485d99d4b3a83c0
Rimma Yatsenko <rmyatsenko@domclick.ru>
2021-11-11 20:17:59 +0300

    step 4 fix2

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 export default function userName() {
   console.log('Welcome to the Brain Games!');
-  const   userName = readlineSync.question('May I have your name? ');
-  console.log('Hello ' + userName + '!');
-};
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${name} !`);
+}
