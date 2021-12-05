f1f2aa0b96f19f1643f17f397c809572dc12eb94
Rimma Yatsenko <rmyatsenko@domclick.ru>
2021-12-05 21:23:33 +0300

    step 5

---
+++ src/games/brain-even.js
@@ -0,0 +1,16 @@
+import { getRandomNumber } from "../common.js";
+import { runGame } from "../index.js";
+
+const isEven = (number) => number % 2 === 0;
+
+const description = 'Answer "yes" if given number is even. Otherwise answer "no".';
+const getGameData = () => {
+    const number = getRandomNumber();
+    const question = `${number}`;
+    const answer = isEven(number) ? 'yes' : 'no';
+    return [question, String(answer)];
+};
+
+const brainEven = () => runGame(description, getGameData);
+
+export default brainEven;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+export const runGame = (description, getGameData) => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello ${name} !`);
+    console.log(` ${description}`);
+    for (let i = 0; i < roundsCount; i += 1) {
+        const [question, answer] = getGameData();
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        if (userAnswer === answer) {
+            console.log('Correct!');
+        } else {
+            console.log(`'${userAnswer}' is wrong answer;(. Correct answer was '${answer}'`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+    }
+    console.log(`Congratulations, ${name}!`);
+};
